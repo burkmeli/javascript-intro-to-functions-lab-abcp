c5aca31e18e446f7dd2a00bd1926cf9638fa33b8
Melinda Burke <melccb@gmail.com>
2018-01-12 05:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper (string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma = (string) => {
+function sayHiToGrandma (string)  {
   console.log('I can\'t hear you');
-  vav lowercase = 'I can\'t hear you';
+  var lowercase = 'I can\'t hear you';
   lowercase.toLowerCase() === lowercase
   
 }
